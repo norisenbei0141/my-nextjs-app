f25127469b8f6e535006bc6f440c87dc1447cec0
wantanmen <norisen0141@icloud.com>
2025-11-18 09:15:28 +0900

    Vercel Speed Insightsを導入

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -3,6 +3,8 @@ import { GeistSans } from "geist/font/sans";
 import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 
 export const metadata: Metadata = {
   title: "My App",
@@ -13,7 +15,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="ja" className={`${GeistSans.variable} ${GeistMono.variable}`}>
       <body>{children}
-      <Analytics />
+      <Analytics /><SpeedInsights />
       </body>
     </html>
   );
